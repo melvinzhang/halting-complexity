382e64e51bccd796edd0c9e1445a7f59eec46877
melvinzhang <melvinzhang@gmail.com>
2019-07-14 12:50:00 +0800

    improve computation of m using bit_length instead of repeated division by 2

--- riemann-hypothesis/rh.py
+++ riemann-hypothesis/rh.py
@@ -25,15 +25,13 @@ from math import gcd
 d=m=p=0
 f0=f1=f3=n=q=1
 while p**2*(m-f0)<f3:
+  print(n,p)
   d=2*n*d-(-1)**n*f1
   n=n+1
-  print(n)
   g=gcd(n,q)
   q=n*q//g
   if g==1: p=p+1
-  m=0; q2=q
-  while q2>1:
-    q2=q2//2; m=m+d
+  m=d*(q.bit_length()-1)
   f1=2*f0
   f0=2*n*f0
   f3=(2*n+3)*f3
